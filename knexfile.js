d951fb491db11f0c6d2ae73ee31d1f91acefb774
Mendes Sousa <leoprofessionallogytech@outlook.com>
2022-08-31 02:55:08 -0300

    back II

--- knexfile.js
+++ knexfile.js
@@ -5,13 +5,19 @@
  * 
  */
 
- let {environment}=require('./.env')
+//  let {environment}=require('./.env')
 
 module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {environment},
+    connection: {
+            host:'ec2-44-210-36-247.compute-1.amazonaws.com',
+            user:'ikfzqcivuwtvnz',
+            password:'42f3bb29149d75581514e0cecf56fbacb594ede500daa15a44137fa7f0361707',
+            database:'d8ki94ot9piqpu',
+            ssl:{ rejectUnauthorized: false }
+    },
     pool: {
       min: 2,
       max: 10
